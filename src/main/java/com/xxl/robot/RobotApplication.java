5b6a593e0b5d52c1231a24be5595e31dee4253bc
xuxuelei <xuxuelei@yunzhichong.com>
2020-06-14 13:55:41 +0800

    1

--- src/main/java/com/xxl/robot/RobotApplication.java
+++ src/main/java/com/xxl/robot/RobotApplication.java
@@ -2,6 +2,7 @@ package com.xxl.robot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +19,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages={"com.xxl.*"})
 public class RobotApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RobotApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(RobotApplication.class);
+		//SpringApplication.run(SystemctlApplication.class, args);
+		builder.headless(false)
+				// .web(WebApplicationType.NONE)
+				// .bannerMode(Banner.Mode.OFF)
+				.run(args);
 	}
 
 
