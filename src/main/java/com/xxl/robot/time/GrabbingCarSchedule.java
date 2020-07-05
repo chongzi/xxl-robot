32b27cbc7b69a87aad3b15520584bbd031c85c18
xuxuelei <xuxuelei@yunzhichong.com>
2020-07-05 16:28:43 +0800

    11

--- src/main/java/com/xxl/robot/time/GrabbingCarSchedule.java
+++ src/main/java/com/xxl/robot/time/GrabbingCarSchedule.java
@@ -45,7 +45,7 @@ public class GrabbingCarSchedule {
      * 表示每隔3分钟获取数据一次
      * 至少大于1分钟
      */
-   @Scheduled(cron = "0 0/3 * * * ?")
+   //@Scheduled(cron = "0 0/3 * * * ?")
     public void collectSource(){
         log.info("********************collectSource()方法源始数据采集信定时器启动**************************");
         RobotConfig robotConfig = robotConfigService.getByConfigNo("39.99.210.127");
@@ -62,7 +62,7 @@ public class GrabbingCarSchedule {
      * 表示每隔3分钟获取数据一次
      * 至少大于1分钟
      */
-  //@Scheduled(cron = "0 0/3 * * * ?")
+  @Scheduled(cron = "0 0/3 * * * ?")
    public void qqCrawl(){
        log.info("********************qqProces定时器启动**************************");
        RobotConfig robotConfig = robotConfigService.getByConfigNo("39.99.210.127");
