2a038c1b510469129d4e96ff958e67bcf821f6e7
xuxuelei <xuxuelei0801@126.com>
2020-07-14 12:55:18 +0800

    12

--- src/main/java/com/xxl/robot/entity/RobotPlan.java
+++ src/main/java/com/xxl/robot/entity/RobotPlan.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 
 @Api(tags="执行计划实体类")
@@ -19,18 +20,23 @@ public class RobotPlan {
 	@ApiModelProperty(value = "主键ID")
 	@Id
 	private Long id;
-	@ApiModelProperty(value="主机ip", required=true)
-	private String host;
-	@ApiModelProperty(value="种类")
-	private String kind;
+	@ApiModelProperty(value="机器人编号", required=true)
+	private String robotCode;
 	@ApiModelProperty(value="种类编码值")
 	private String cron;
-	@ApiModelProperty(value="顺序号")
-	private Byte operate;
+	@ApiModelProperty(value="操作方式（0-QQ数据采集，1-QQ自动发送，2-QQ群、好友添加 ，3-微信采集，4-微信自动发送，5-微信添加）")
+	private Byte operateType;
+	@ApiModelProperty(value="登录账号(如网站登录名,QQ或微信登录账号）")
+	private String account;
+	@ApiModelProperty(value="密码")
+	private String pwd;
+	@ApiModelProperty(value="工具地址(网站地址,QQ或微信安装目录bin)")
+	private String url;
 	@ApiModelProperty(value="是否启用（0-启用，1-禁用）", required=true)
 	private Byte enabled;
 	@ApiModelProperty(value="备注")
 	private String remark;
-
+	@ApiModelProperty(value="创建时间")
+	private Date createDate;
 
 }
